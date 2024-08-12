965030072cef21516595ef37315ae8c82dc7b388
Steve <stchonet@gmail.com>
2024-08-12 09:44:12 -0500

    terraform week10

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "ws73-st-terraform" #replace with your bucket
+    bucket         = "whfdrtstrgdghdf" #replace with your bucket
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "locktable" #replace with the table
+    dynamodb_table = "hjfghdhtsshhg" #replace with the table
   }
 }
